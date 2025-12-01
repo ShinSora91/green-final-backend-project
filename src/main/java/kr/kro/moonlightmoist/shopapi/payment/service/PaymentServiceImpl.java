c8a077a740c991ee9af698812bcb90ef811a83fe
minsuk <kmsk0802@gmail.com>
2025-12-01 22:51:58 +0900

    Service 어노테이션 추가
    - PaymentServiceImpl에 Service 어노테이션 추가

--- src/main/java/kr/kro/moonlightmoist/shopapi/payment/service/PaymentServiceImpl.java
+++ src/main/java/kr/kro/moonlightmoist/shopapi/payment/service/PaymentServiceImpl.java
@@ -1,5 +1,8 @@
 package kr.kro.moonlightmoist.shopapi.payment.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PaymentServiceImpl implements PaymentService {
 
 }
